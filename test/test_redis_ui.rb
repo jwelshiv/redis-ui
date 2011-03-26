47ac89e967fd73589b6d02b1b2bea0357577dcb8
jwelshiv <james@supermatter.com>
2011-03-26 15:12:05 -0700

    cleans up old naming "rediseer"

--- test/test_redis_ui.rb
+++ test/test_redis_ui.rb
@@ -1,6 +1,6 @@
 require 'helper'
 
-class TestRediseer < Test::Unit::TestCase
+class TestRedisUI < Test::Unit::TestCase
   should "probably rename this file and start testing for real" do
     flunk "hey buddy, you should probably rename this file and start testing for real"
   end
