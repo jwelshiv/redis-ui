76b4d4d2bbe760f1ba6f13793c332e2a3266ec57
Jeff Gran <jeff.gran@openlogic.com>
2012-06-19 21:35:48 -0600

    add ability to delete keys from UI

--- lib/redis-ui.rb
+++ lib/redis-ui.rb
@@ -3,6 +3,7 @@ require 'redis'
 require 'redis/namespace'
 require 'deep_merge'
 require 'pp'
+require 'cgi'
 
 begin
   require 'yajl'
@@ -13,6 +14,8 @@ end
 require 'sinatra/base'
 require 'sinatra/respond_to'
 require 'erb'
+
+require 'redis-ui/helpers'
 
 module RedisUI
   extend self
@@ -64,81 +67,7 @@ module RedisUI
     helpers do 
       include Rack::Utils
       alias_method :h, :escape_html
-
-      def redis
-        RedisUI.redis
-      end
-      
-      def get_key(key)
-        data = case redis.type(key)
-               when "string"
-                 redis[key]
-               when "list"
-                 redis.lrange(key, 0, -1)
-               when "set"
-                 redis.smembers(key)
-               when 'zset'
-                 redis.zrange(key, 0, -1)
-               when 'hash'
-                 redis.hgetall(key)
-               else
-                 '???'
-               end
-
-        {:key => key, :type => redis.type(key), :data => data}
-      end
-
-      def show(val)
-        case val
-        when String
-          val
-        when Array
-          if val.empty?
-            '[]'
-          else
-            str = "<ul><li> · "
-            str << val.map{|v|show(v)}.join('</li><li> · ')
-            str << '</li></ul>'
-          end
-        when Hash
-          str = "<ul><li> · "
-          arr = []
-          val.map do |k, v|
-            arr << "#{k} => #{show(v)}"
-          end
-          str << arr.join("</li><li> · ")
-          str << '</li></ul>'
-        else
-          val.to_s
-        end
-      end
-
-      def render_tree(val)
-          str = "<ul>"
-          val.each do |k, v|
-            str << "<li><a href='#'>#{k}</a>#{render_tree(v)}</li>"
-          end
-          str << '</ul>'
-      end
-
-      def build_namespace_tree
-        @namespace_tree = {}
-        namespaces = redis.keys.map{|k| k.split(':')[0..-2]}.uniq.sort_by(&:size)
-        namespaces.each do |array|
-          hash = array.reverse.inject({}) do |branch, string|
-            newtrunk = {}
-            newtrunk[string] = branch
-            newtrunk
-          end
-          @namespace_tree.deep_merge!(hash)
-        end
-      end
-      # {
-      #   'top' => {
-      #     'second' => {}
-      #   }
-      # }
-      
+      include RedisUIHelpers
     end
     
     get "/" do
@@ -160,6 +89,15 @@ module RedisUI
       respond_to do |wants|
          wants.html { erb :show }      # => views/posts.html.haml, also sets content_type to text/html
          wants.js { @data.to_json }
+      end
+    end
+
+    post "/del/:key" do
+      result = redis.del(params[:key])
+      if result == 1
+        true
+      else
+        500
       end
     end
 
